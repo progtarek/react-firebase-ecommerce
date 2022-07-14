68e3252525a2cd7b27f61883637c205b6a9b69d8
Tarek Noaman <progtarek@gmail.com>
2022-07-14 17:26:30 +0200

    Reducers | Add users reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,12 +1,36 @@
-const { useState, createContext } = require("react");
+const { createContext, useReducer } = require("react");
 
-export const UserContext = createContext({
+const USER_ACTIONS = {
+  SET_CURRENT_USER: "SET_CURRENT-USER",
+};
+
+const userReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTIONS.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+
+    default:
+      throw Error("Invalid action type");
+  }
+};
+
+const initialState = {
   currentUser: null,
   setCurrentUser: () => null,
-});
+};
+
+export const UserContext = createContext(initialState);
 
 export const UserContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, initialState);
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTIONS.SET_CURRENT_USER, payload: user });
+  };
   const value = { currentUser, setCurrentUser };
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
